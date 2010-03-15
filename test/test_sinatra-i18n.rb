1860365328e7d09a742126a41a58e54fa3b8c2a7
Cyril David <cyx.ucron@gmail.com>
2010-03-16 03:21:21 +0800

    Initial commit to sinatra-i18n.

---
+++ test/test_sinatra-i18n.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestSinatraI18n < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
