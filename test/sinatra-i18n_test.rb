b5d0571cd57abe50c7a03d7b0a0a35d4a655e0ee
Stephane D'Alu <sdalu@sdalu.com>
2013-03-01 17:18:49 +0100

    tests

--- test/sinatra-i18n_test.rb
+++ test/sinatra-i18n_test.rb
@@ -1,7 +1,44 @@
 require File.expand_path('../helper', __FILE__)
 
+require 'sinatra/base'
+require 'sinatra/i18n'
+
 class TestSinatraI18n < Test::Unit::TestCase
-    test 'all good' do
-        assert true
+    test 'translation for en' do
+        i18n_app_result(:en) { t(:'world') }
+        assert body == 'World'
+    end
+
+
+    test 'translation for fr' do
+        i18n_app_result(:fr) { t(:'world') }
+        assert body == 'Monde'
+    end
+
+
+
+    def i18n_app(&block) 
+        mock_app do
+            register Sinatra::I18n 
+
+            configure do
+                I18n.load_path = Dir[File.join(File.dirname(__FILE__),
+                                               'locales', '*.yml')]
+                I18n.backend   = I18n::Backend::Simple.new
+                I18n.backend.load_translations
+            end
+
+            instance_eval(&block)
+        end
+    end
+
+    def i18n_app_result(locale = :en, &block)
+        i18n_app do
+            get '/' do
+                I18n.locale = locale
+                instance_eval(&block)
+            end
+        end
+        get '/'
     end
 end
